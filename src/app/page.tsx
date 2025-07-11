c89e7d8c24cdbf18787fa339b94ae9d472bab9e9
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-07-11 16:02:57 +0300

    Addd base stucture, styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
+	<main>
+		<h1 className="title">Product Catalog</h1>
 
-	<h1>Product Catalog</h1>
+		<div className="sorting">
+			<h2>Sorting</h2>
+		</div>
 
+		<div className="filtering">
+			<h2>Filtering</h2>
+		</div>
+
+		<div className="cards">
+			<h2>Product Cards</h2>
+		</div>
+	</main>
   )
 }
