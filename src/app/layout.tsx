dc251f0990c31c2129334bdee998f978da20e165
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-07-20 15:23:48 +0300

    Add fonts into globals.scss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
 import "./globals.scss";
+import { Arimo } from 'next/font/google';
+
+const arimo = Arimo({
+	subsets: ['latin'],
+  	display: 'swap',
+	variable: '--font-arimo',
+})
 
 export default function RootLayout({
   children,
@@ -6,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={arimo.className}>
       <body>
         {children}
       </body>
