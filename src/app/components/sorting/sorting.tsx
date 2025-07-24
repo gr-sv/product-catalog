6970fa111523d97750835a9c5fff60294449df07
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-07-24 22:22:43 +0300

    Add styles for Sorting, corrections

--- src/app/components/sorting/sorting.tsx
+++ src/app/components/sorting/sorting.tsx
@@ -1,3 +1,5 @@
+import s from './sorting.module.scss';
+
 interface SortingProps {
   className?: string;
 }
@@ -5,15 +7,15 @@ interface SortingProps {
 export default function Sorting ({ className }: SortingProps) {
 	return (
 
-		<label className={className}>
+		<div className={className}>
 
-			<select name="sorting">
+			<select className={s.sortingValues} name="sorting">
 				<option value="high to low">Price: high to low</option>
 				<option value="low to high">Prive: low to high</option>
 				<option value="rating">Rating</option>
 				<option value="naming">Naming</option>
 			</select>
 
-		</label>
+		</div>
 	)
 }
