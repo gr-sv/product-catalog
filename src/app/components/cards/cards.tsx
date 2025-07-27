06a53664b1f9eaf73362040177f600107a88f93c
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-07-27 16:20:23 +0300

    Modify Cards

--- src/app/components/cards/cards.tsx
+++ src/app/components/cards/cards.tsx
@@ -11,12 +11,14 @@ export default function Cards ({ className }: CardsProps) {
 		<div className={className}>
 			{(data?.products).map(product => (
 				<div key={product.id} className={s.card}>
-					<h2 className={s.cardTitle}>{product.name}</h2>
-					<p className={s.cardDescription}>{product.description}</p>
-					<p className={s.cardPrice}>{`${product.price} $`}</p>
-					<p className={s.cardRating}>&#9733; {product.rating}</p>
 					<img className={s.cardImage} src={product.image.desktop} />
-					<p className={s.cardCategory}>{`# ${product.category}`}</p>
+					<div className={s.cardAbout}>
+						<h2 className={s.cardTitle}>{product.name}</h2>
+						<p className={s.cardDescription}>{product.description}</p>
+						<p className={s.cardPrice}>{`${product.price} $`}</p>
+						<p className={s.cardRating}>&#9733; {product.rating}</p>
+						<p className={s.cardCategory}>{`# ${product.category}`}</p>
+					</div>
 				</div>
 			))}
 		</div>
