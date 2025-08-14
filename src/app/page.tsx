b9a842c89b10864abcb8c6910c906600df9d727c
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-08-14 22:44:29 +0300

    Fix mistakes in Page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,31 @@
+'use client'
+
+import { useState } from 'react';
 import Filtering from './components/filtering/filtering';
 import Sorting from './components/sorting/sorting';
 import Cards from './components/cards/cards';
 
 export default function Home() {
-  return (
-	<main className='main'>
-		<h1 className="title">Product Catalog</h1>
 
-		<Sorting className="sorting"/>
+	const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
 
-		<Filtering className="filtering"/>
+	console.log('selectedCategories', selectedCategories)
 
-		<Cards className="cards"/>
-	</main>
+	return (
+		<main className='main'>
+			<h1 className="title">Product Catalog</h1>
+
+			<Sorting className="sorting"/>
+
+			<Filtering
+				className="filtering"
+				selected={selectedCategories}
+            	setSelected={setSelectedCategories}
+			/>
+
+			<Cards
+				className="cards"
+				selectedCategories={selectedCategories}/>
+		</main>
   )
 }
